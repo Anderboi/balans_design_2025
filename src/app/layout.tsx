f6cc097099e4046cc7b58b3e276a0cd44ee812c7
Anderboi <Upolovnew@gmail.com>
2025-11-03 10:20:16 +0300

    feat: shadcn sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "@/components/sidebar";
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import AppSidebar from '@/components/app-sidebar';
 
 const inter = Inter({ subsets: ["cyrillic"] });
 
@@ -19,8 +21,12 @@ export default function RootLayout({
     <html lang="ru">
       <body className={inter.className}>
         <div className="flex h-screen">
-          <Sidebar />
-          <main className="flex-1 overflow-auto p-6">{children}</main>
+          <SidebarProvider>
+            <AppSidebar/>
+            {/* <Sidebar /> */}
+            <SidebarTrigger />
+            <main className="flex-1 overflow-auto py-6 pr-6">{children}</main>
+          </SidebarProvider>
         </div>
       </body>
     </html>
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import {
+  Book,
+  BriefcaseBusiness,
+  ChevronUp,
+  Home,
+  Image,
+  Package,
+  SquareUser,
+  UserCircleIcon,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+//? Menu items
+const mainItems = [
+  {
+    title: "Дашборд",
+    href: "/",
+    icon: Home,
+  },
+  {
+    title: "Проекты",
+    href: "/projects",
+    icon: BriefcaseBusiness,
+  },
+];
+const libraryItems = [
+  {
+    title: "Материалы",
+    href: "/materials",
+    icon: Package,
+  },
+  {
+    title: "Контакты",
+    href: "/contacts",
+    icon: SquareUser,
+  },
+  {
+    title: "База знаний",
+    href: "/knowledge",
+    icon: Book,
+  },
+  {
+    title: "Галлерея изображений",
+    href: "/gallery",
+    icon: Image,
+  },
+];
+
+const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <h1 className="text-3xl font-bold p-2">BALANS</h1>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {mainItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild size={"sm"}>
+                    <a href={item.href}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Библиотеки</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {libraryItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild size={"sm"}>
+                    <a href={item.href}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton size={"lg"}>
+                  <UserCircleIcon />
+                  UserName
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Профиль</span>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <span>Выйти</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
