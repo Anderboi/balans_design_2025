d476378904152c6539dadb5e73f99730a3db0385
Anderboi <Upolovnew@gmail.com>
2026-04-08 11:16:38 +0300

    feat: optimisation files

---
+++ src/app/(marketing)/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl =
+    process.env.APP_URL || "https://balans-design-2025.vercel.app";
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
